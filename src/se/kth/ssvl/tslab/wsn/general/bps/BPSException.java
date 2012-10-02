f0cbf6af70ed1637ba41ecf16d1b9b4f889e8c97
Konstantinos Vaggelakos <kozze89@gmail.com>
2012-10-02 15:58:43 +0200

    Generated serial version UID for bps exception

--- src/se/kth/ssvl/tslab/wsn/general/bps/BPSException.java
+++ src/se/kth/ssvl/tslab/wsn/general/bps/BPSException.java
@@ -2,6 +2,8 @@ package se.kth.ssvl.tslab.wsn.general.bps;
 
 public class BPSException extends Exception {
 
+	private static final long serialVersionUID = -468937647917398954L;
+
 	public BPSException(String message) {
 		super(message);
 	}
