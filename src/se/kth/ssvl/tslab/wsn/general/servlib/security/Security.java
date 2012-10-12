e5000280c68e1180cbbf86220fd969764017ddb0
Fabio Viggiani <viggiani.fabio@gmail.com>
2012-10-12 10:52:35 +0200

    Added security classes for PCB blocks. Fixed logging.

--- src/se/kth/ssvl/tslab/wsn/general/servlib/security/Security.java
+++ src/se/kth/ssvl/tslab/wsn/general/servlib/security/Security.java
@@ -1,20 +1,23 @@
 package se.kth.ssvl.tslab.wsn.general.servlib.security;
 
+import se.kth.ssvl.tslab.wsn.general.bpf.BPF;
+import se.kth.ssvl.tslab.wsn.general.servlib.bundling.BlockInfo;
 import se.kth.ssvl.tslab.wsn.general.servlib.bundling.BlockInfoVec;
 import se.kth.ssvl.tslab.wsn.general.servlib.bundling.Bundle;
 import se.kth.ssvl.tslab.wsn.general.servlib.contacts.Link;
 import se.kth.ssvl.tslab.wsn.general.servlib.config.DTNConfiguration;
+import se.kth.ssvl.tslab.wsn.general.servlib.security.SPD.spd_policy_t;
 
 /**
- * This class is used as entry point to all security classes.
+ * TODO
  * 
  */
 
 //Access point to the security part
 public class Security {
 	
-	private static Security instance;
-	
+	private static String TAG = "Security";
+	/*
 	private String ks_path_;
 	private String ks_password_;
 	private String use_pcb_;
@@ -37,34 +40,49 @@ public class Security {
 	public String use_pib() {
 		return use_pib_;
 	}
+	*/
 	
 	/**
-	 * This method will return an singleton instance of Security
-	 * @return The Security singleton
+	 * Constructor: read config instance and store security settings locally. Other security classes will access these.
+	 * It also initializes default ciphersuites.
 	 */
-	public static Security getInstance() {
-		if (instance == null) {
-			instance = new Security();
-		}
-		return instance;
-	}
-	
-	public void init(DTNConfiguration config_){//TODO
+	public Security(){
 		//init SPD?
 		//init KeyDB?
 		Ciphersuite.init_default_ciphersuites();
 		
-		//here we get everything that we need from the config file and store it in local variables
-		ks_path_ = config_.security_setting().ks_path();
-		ks_password_ = config_.security_setting().ks_password();
-		use_pcb_ = config_.security_setting().use_pcb();
-		use_pib_ = config_.security_setting().use_pib();
-		
+		/*
+		ks_path_ = DTNConfiguration.getInstance().security_setting().ks_path();
+		ks_password_ = DTNConfiguration.getInstance().security_setting().ks_password();
+		use_pcb_ = DTNConfiguration.getInstance().security_setting().use_pcb();
+		use_pib_ = DTNConfiguration.getInstance().security_setting().use_pib();
+		*/
+		BPF.getInstance().getBPFLogger().debug(TAG, "Initialized.");
 	}
 	
-	public void prepare_out_blocks(Bundle bundle, Link link, BlockInfoVec xmit_blocks){//TODO
+	//This method will be called from BundleProtocol.prepare_blocks()
+	public void prepare_out_blocks(Bundle bundle, Link link, BlockInfoVec xmit_blocks){
+		int err = 0;
 		
-		SPD.getInstance().prepare_out_blocks(bundle, link, xmit_blocks); 
+		//Do we have to use PCB?
+		if(DTNConfiguration.getInstance().security_settings().use_pcb().equals("true")) 
+		{
+			Ciphersuite bp =  Ciphersuite.find_suite(Ciphersuite_C3.CSNUM_C3);
+			assert(bp != null);
+			err = bp.prepare(bundle, xmit_blocks, null, link, BlockInfo.list_owner_t.LIST_NONE);
+		}
+		
+		//Do we have to use PIB?
+		if(DTNConfiguration.getInstance().security_settings().use_pib().equals("true"))
+		{
+			BPF.getInstance().getBPFLogger().error(TAG, "PIB blocks are not supported yet!");
+			//TODO: Add PIB support!
+		}
+		
+		if (err != 0)
+			BPF.getInstance().getBPFLogger().error(TAG, "Error while preparing security blocks!");
+		else
+			BPF.getInstance().getBPFLogger().debug(TAG, "Blocks have been prepared.");
 	}
 	
 }
