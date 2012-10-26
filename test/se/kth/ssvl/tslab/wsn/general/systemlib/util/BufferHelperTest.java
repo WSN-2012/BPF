7aa8bf4b342aa0707b0270194e2526d9f00fe185
Konstantinos Vaggelakos <kozze89@gmail.com>
2012-10-26 10:48:53 +0200

    Added first test class

---
+++ test/se/kth/ssvl/tslab/wsn/general/systemlib/util/BufferHelperTest.java
@@ -0,0 +1,14 @@
+package se.kth.ssvl.tslab.wsn.general.systemlib.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BufferHelperTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
