1deb629d63f4ba907f5bf7df716c73bb22dbc7e3
Konstantinos Vaggelakos <kozze89@gmail.com>
2012-11-07 14:08:17 +0100

    Added skeleton methods in the epidemic routing class

--- src/se/kth/ssvl/tslab/wsn/general/servlib/routing/routers/EpidemicBundleRouter.java
+++ src/se/kth/ssvl/tslab/wsn/general/servlib/routing/routers/EpidemicBundleRouter.java
@@ -1,5 +1,7 @@
 package se.kth.ssvl.tslab.wsn.general.servlib.routing.routers;
 
+import se.kth.ssvl.tslab.wsn.general.servlib.bundling.bundles.Bundle;
+import se.kth.ssvl.tslab.wsn.general.servlib.bundling.event.ContactUpEvent;
 import se.kth.ssvl.tslab.wsn.general.servlib.reg.Registration;
 
 public class EpidemicBundleRouter extends TableBasedRouter {
@@ -9,4 +11,12 @@ public class EpidemicBundleRouter extends TableBasedRouter {
 		return null;
 	}
 
+	protected void handle_contact_up(ContactUpEvent event) {
+		
+	}
+	
+	
+	public void deliver_bundle(Bundle bundle) {
+		
+	}
 }
