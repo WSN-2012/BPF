a382db7014fbfc83d6b8da3ee5870d12c4d1830d
Konstantinos Vaggelakos <kozze89@gmail.com>
2012-11-08 09:05:36 +0100

    Added database support for bundle hashing and a method for getting all the hashes from the database.

--- src/se/kth/ssvl/tslab/wsn/general/servlib/storage/BundleStore.java
+++ src/se/kth/ssvl/tslab/wsn/general/servlib/storage/BundleStore.java
@@ -23,6 +23,8 @@ package se.kth.ssvl.tslab.wsn.general.servlib.storage;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import se.kth.ssvl.tslab.wsn.general.servlib.bundling.bundles.Bundle;
 import se.kth.ssvl.tslab.wsn.general.servlib.bundling.bundles.BundlePayload;
@@ -60,7 +62,7 @@ public class BundleStore {
 	 * SQL Query for creating new Bundle in the SQLite database.
 	 */
 	private static final String Table_CREATE_BUNDLES = "create table IF NOT EXISTS  bundles (id integer primary key autoincrement, "
-			+ "type integer not null," + "forwarded_times integer);";
+			+ "type integer not null, hash char(40), forwarded_times integer);";
 
 	/**
 	 * Singleton Implementation Getter function
@@ -134,6 +136,30 @@ public class BundleStore {
 		return impt_storage_.get_file(payload_filname);
 	}
 
+	public String[] getBundleList() {
+		try {
+			BPF.getInstance().getBPFLogger().debug(TAG, "Getting bundle list from our storage");
+			
+			List<Map<String, Object>> hashes = BPF.getInstance().getBPFDB().query(table, new String[]{"hash"},
+					null, null, null, null, null, null);
+			
+			String[] ret = new String[hashes.size()];
+			
+			Iterator<Map<String, Object>> i = hashes.iterator();
+			int c = 0;
+			Map<String, Object> item;
+			while(i.hasNext()) {
+				item = i.next();
+				ret[c++] = (String) item.get("hash"); 
+			}
+			
+			return ret;
+		} catch (Exception e) {
+			BPF.getInstance().getBPFLogger().error(TAG, "Couldn't get the bundle list (of hashes):" + e.toString());
+		}
+		return null;
+	}
+	
 	/**
 	 * Store the new bundle on the disk.
 	 * 
