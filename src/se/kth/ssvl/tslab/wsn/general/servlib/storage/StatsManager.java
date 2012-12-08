fc6acb2672265d171bcc0133b02208cbd5ab3d98
Fabio Viggiani <viggiani.fabio@gmail.com>
2012-12-08 20:46:42 +0100

    Modified updating of statistics

--- src/se/kth/ssvl/tslab/wsn/general/servlib/storage/StatsManager.java
+++ src/se/kth/ssvl/tslab/wsn/general/servlib/storage/StatsManager.java
@@ -133,7 +133,7 @@ public class StatsManager {
 			return;
 		}
 
-		//update private variables
+		// update private variables
 		if (stat.equals("size")) {
 			size = value;
 		} else if (stat.equals("stored")) {
@@ -143,13 +143,21 @@ public class StatsManager {
 		} else if (stat.equals("received")) {
 			received = value;
 		}
-		
-		//inform the Service that new statistics are available
-		BPF.getInstance().updateStats(new Stats(size,stored,transmitted,received));
+
+		// inform the Service that new statistics are available
+		BPF.getInstance().updateStats(
+				new Stats(size, stored, transmitted, received));
 	}
 
 	public void increase(String stat) {
-		if (!stat.equals("transmitted") && !stat.equals("received")) {
+
+		String condition = "";
+		
+		if (stat.equals("transmitted")) {
+			condition = "stat = 'transmitted'";
+		} else if (stat.equals("received")) {
+			condition = "stat = 'received'";
+		} else {
 			BPF.getInstance()
 					.getBPFLogger()
 					.warning(
@@ -160,13 +168,14 @@ public class StatsManager {
 			return;
 		}
 
-		String condition = "stat = '" + stat + "'";
-
 		int current = impt_sqlite_.get_record(table, condition, "value", null);
+		
+		BPF.getInstance().getBPFLogger().error(TAG, "Got stat: " + stat + ". Value: " + current);
+		
 		if (current == -1) {
 			BPF.getInstance()
 					.getBPFLogger()
-					.warning(
+					.error(
 							TAG,
 							"Trying to increase stat: "
 									+ stat
